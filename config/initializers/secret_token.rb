e86cc68345d1e0955a115dccf5ae0bb47b65d2c7
ayumi5 <udakaayumi@udakaayumi-no-MacBook-Air.local>
2014-04-08 09:36:32 +1200

    initial committ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labrat::Application.config.secret_key_base = '75bb9212bf4154f6535a802af19db71ef47babba61bdc07ae1465e6860e8d77f8715c443edd5b9040090a65eefb88a9865ae137231b740b8db87c0c9589eea65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Labrat::Application.config.session_store :cookie_store, key: '_Labrat_session'
